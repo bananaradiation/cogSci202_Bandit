0e610008b2eaaa6a7ebd2a5e17b2b3c7e89c2af7
bananaradiation <hannahelainechen@gmail.com>
2014-05-20 16:41:11 -0700

    rename var

--- enumStates.m
+++ enumStates.m
@@ -5,9 +5,9 @@ function [FS] = enumStates(trials,arms)
     k = arms*2;
     % All possible placements of internal dividers.
     dividers = nchoosek(1:(t+k-1), k-1);
-    ndividers = size(dividers, 1);
+    numStates = size(dividers, 1);
     % Add dividers at the beginning and end.
-    b = cat(2, zeros(ndividers, 1), dividers, (t+k)*ones(ndividers, 1));
+    b = cat(2, zeros(numStates, 1), dividers, (t+k)*ones(numStates, 1));
     % Find distances between dividers.
     FS = diff(b, 1, 2) - 1;
 end
