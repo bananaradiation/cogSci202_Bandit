91e3fb65d3833006e03fcba5018c6846ea005fdf
yashodhan <yashkarandikar@gmail.com>
2014-05-20 17:59:20 -0700

    Fixed bug in findBestEpsilonValue; changed variable name arms

--- simulateEpsilonGreedy.m
+++ simulateEpsilonGreedy.m
@@ -1,8 +1,8 @@
 % runs the epsilon-greedy model for T number of trials and returns the a column vector of decisions taken by the model
-function decisions = simulateEpsilonGreedy(T, arms, epsilon)
+function decisions = simulateEpsilonGreedy(T, armDistributions, epsilon)
     % arms : column vector of 2 elements containing mu_1 and mu_2 (Bernoulli distributions of the 2 arms)
 
-    numArms = size(arms,1);
+    numArms = size(armDistributions,1);
     s = zeros(numArms, 1); % # successes from arms
     n = zeros(numArms, 1);  % # trials where arm 1 was chosen
     
@@ -28,7 +28,7 @@ function decisions = simulateEpsilonGreedy(T, arms, epsilon)
         end
         decisions(t) = choice;    % update decisions vector
         n(choice) = n(choice) + 1;        % update n vector
-        reward = selectArm(arms(choice));    % sample reward from the mu of the arm
+        reward = selectArm(armDistributions(choice));    % sample reward from the mu of the arm
         s(choice) = s(choice) + reward;    % update success vector
     end
 end
