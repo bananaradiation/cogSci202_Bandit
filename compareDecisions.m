904a944174e4bb941530266f787e5afa2ec13d88
yashodhan <yashkarandikar@gmail.com>
2014-05-20 17:53:40 -0700

    Fixed errors in epsilon greedy code

--- compareDecisions.m
+++ compareDecisions.m
@@ -1,4 +1,4 @@
 % compares 2 column vectors representing decisions and returns a percentage of matches
 function percentMatch = compareDecisions(decVector1, decVector2)
-    sum(decVector1 == decVector2) / size(decVector1,1);
+    percentMatch = sum(decVector1 == decVector2) / size(decVector1,1);
 end
--- simulateEpsilonGreedy.m
+++ simulateEpsilonGreedy.m
@@ -1,38 +1,34 @@
 % runs the epsilon-greedy model for T number of trials and returns the a column vector of decisions taken by the model
 function decisions = simulateEpsilonGreedy(T, arms, epsilon)
-    s1 = 0;    % # successes from arm 1
-    f1 = 0;    % # failures from arm 1
-    s2 = 0;    % # successes from arm 2
-    f2 = 0;    % # failures from arm 2
+    % arms : column vector of 2 elements containing mu_1 and mu_2 (Bernoulli distributions of the 2 arms)
+
+    numArms = size(arms,1);
+    s = zeros(numArms, 1); % # successes from arms
+    n = zeros(numArms, 1);  % # trials where arm 1 was chosen
+    
+    decisions = zeros(T,1);    
+    
     for t = 1 : T
-        % with probability epsilon, select randomly
-        r = rand();
-        if (r < 0.5
-    end
-end
-
-function bestEps = findEpsilonValue(T,optimalDecisions, arms)
-    bestMatchPercent = 0;
-    bestEps = 0;
-    
-    % grid search over epsilon values
-    for e = 0 : 0.001 : 1
-        
-        % for each value of epsilon, run epsilon-greedy model and get decisions
-        eGreedyDecisions = simulateEpsilonGreedy(T, arms);
-        
-        % compare decisions with optimal decisions
-        matchPercent = compareDecisions(eGreedyDecisions, optimalDecisions);
-        if (matchPercent < bestPercent)
-            bestPercent = matchPercent;
-            bestEps = e;
+        r = rand(); 
+        if (r < epsilon)
+            % with probability epsilon, select randomly
+            r = rand();
+            if (r > 0.5)
+                choice = 1;         % choose arm 1       
+            else
+                choice = 2;    % choose arm 2
+            end
+        else
+            % with probability 1 - epsilon, exploit based on experience so far
+            if ((s(1) / n(1)) > (s(2) / n(2)))                
+                choice = 1;        % choose arm 1
+            else                
+                choice = 2;        % choose arm 2
+            end
         end
+        decisions(t) = choice;    % update decisions vector
+        n(choice) = n(choice) + 1;        % update n vector
+        reward = selectArm(arms(choice));    % sample reward from the mu of the arm
+        s(choice) = s(choice) + reward;    % update success vector
     end
 end
-
-% compares 2 column vectors representing decisions and returns a percentage of matches
-function percentMatch = compareDecisions(decVector1, decVector2)
-    sum(decVector1 == decVector2) / size(decVector1,1);
-end
-    
-
