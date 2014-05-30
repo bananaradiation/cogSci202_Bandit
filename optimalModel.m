41db375cc3816af64a689360bc009056658903fc
hannahchen <bananaradiation@gmail.com>
2014-05-29 17:44:39 -0700

    incorporate mu as laster term in value function

--- optimalModel.m
+++ optimalModel.m
@@ -1,5 +1,7 @@
 function [valueMatrix decisionMatrix] = optimalModel(T,arms,alpha,beta)
 
+    mu1 = betarnd(alpha,beta);
+    mu2 = betarnd(alpha,beta);
     deductionVector = [ [-1,0,0,0]; [0,-1,0,0]; [0,0,-1,0]; [0,0,0,-1]];
 
     valueMatrix = zeros(9,9,9,9);
@@ -27,9 +29,9 @@ function [valueMatrix decisionMatrix] = optimalModel(T,arms,alpha,beta)
                
                totalArm1 = s1+f1+alpha+beta;
                totalArm2 = s2+f2+alpha+beta;
-               arm1Value = ((s1+alpha)/(totalArm1)) * valueMatrix(s1+1,idxS2,idxF1,idxF2) + ((f1+beta)/(totalArm1)) * valueMatrix(idxS1,idxS2,f1+1,idxF2) + ((s1+alpha)/(totalArm1));
+               arm1Value = ((s1+alpha)/(totalArm1)) * valueMatrix(s1+1,idxS2,idxF1,idxF2) + ((f1+beta)/(totalArm1)) * valueMatrix(idxS1,idxS2,f1+1,idxF2) + mu1;
                
-               arm2Value = ((s2+alpha)/(totalArm2)) * valueMatrix(idxS1,s2+1,idxF1,idxF2) + ((f2+beta)/(totalArm2)) * valueMatrix(idxS1,idxS2,idxF1,f2+1) + ((s2+alpha)/(totalArm2));
+               arm2Value = ((s2+alpha)/(totalArm2)) * valueMatrix(idxS1,s2+1,idxF1,idxF2) + ((f2+beta)/(totalArm2)) * valueMatrix(idxS1,idxS2,idxF1,f2+1) + mu2;
                
                [valueMatrix(idxS1,idxS2,idxF1,idxF2), decisionMatrix(idxS1,idxS2,idxF1,idxF2)] = max([arm1Value,arm2Value]); 
            end  
