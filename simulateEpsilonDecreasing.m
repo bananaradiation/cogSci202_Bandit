b8105e7cb6de4c13fdcc4a6c6f1ae605c561a6ea
Yashodhan Karandikar <yashkarandikar@gmail.com>
2014-05-29 18:42:27 -0700

    Adding epsilon decreasing, fixed bug in epsilon greedy

--- simulateEpsilonDecreasing.m
+++ simulateEpsilonDecreasing.m
@@ -1,15 +1,16 @@
-% runs the epsilon-greedy model for T number of trials and returns the a column vector of decisions taken by the model
-function decisions = simulateEpsilonGreedy(T, armDistributions, epsilon)
-    % arms : column vector of 2 elements containing mu_1 and mu_2 (Bernoulli distributions of the 2 arms)
+% runs the epsilon-decreasing model for T number of trials and returns the a column vector of decisions taken by the model
+function decisions = simulateEpsilonDecreasing(T, armDistributions, epsilon0)
+    % armDistributions : column vector of 2 elements containing mu_1 and mu_2 (Bernoulli distributions of the 2 arms)
+    % epsilon0 : the initial value of epsilon
 
-    numArms = size(armDistributions,1);
+    numArms = size(armDistributions,1); 
     s = zeros(numArms, 1); % # successes from arms
     n = zeros(numArms, 1);  % # trials where arm 1 was chosen
     
-    decisions = zeros(T,1);    
-    
+    decisions = zeros(T,1);        
     for t = 1 : T
-        r = rand();     % decide whether to explore randomly or exploit
+        epsilon = epsilon0 / t;
+        r = rand();     % decide whether to explore randomly or exploit        
         if (r < epsilon)
             % with probability epsilon, select randomly
             %r = rand();
@@ -25,7 +26,8 @@ function decisions = simulateEpsilonGreedy(T, armDistributions, epsilon)
         end
         decisions(t) = choice;    % update decisions vector
         n(choice) = n(choice) + 1;        % update n vector
-        reward = selectArm(armDistributions(choice));    % sample reward from the mu of the arm
+        %reward = selectArm(armDistributions(choice));    % sample reward from the mu of the arm
+        reward = binornd(1,armDistributions(choice));
         s(choice) = s(choice) + reward;    % update success vector
     end
 end
