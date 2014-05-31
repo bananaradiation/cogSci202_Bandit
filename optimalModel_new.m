009aeb6fdf042d3e46cda56f20278eda417f19df
Yashodhan Karandikar <yashkarandikar@gmail.com>
2014-05-30 19:40:36 -0700

    Fixed bug in optimal model new

--- optimalModel_new.m
+++ optimalModel_new.m
@@ -11,20 +11,23 @@ function [decisions,rewards, mu1, mu2] = optimalModel(T,arms,alpha,beta)
     [allStates, numEndStates] = enumStates(T,arms);    
     for i = 1 : numEndStates
         state = allStates(i,:);                
-        s1 = state(1); f1 = state(2); s2 = state(3); f2 = state(4);
+        s1 = state(1); s2 = state(2); f1 = state(3); f2 = state(4);
         p(1) = (s1 + alpha) / (s1 + f1 + alpha + beta);
         p(2) = (s2 + alpha) / (s2 + f2 + alpha + beta);
         u = s1 + s2 + p;
         state(state == 0) = 9;
-        s1 = state(1); f1 = state(2); s2 = state(3); f2 = state(4);
-        valueMatrix(s1,f1,s2,f2) = max(u);
+        idxS1 = state(1); idxS2 = state(2); idxF1 = state(3); idxF2 = state(4);
+        valueMatrix(idxS1,idxS2,idxF1,idxF2) = max(u);
+        %fprintf('Se');
     end
 
     % recursive formulation for remaining states
-    for t = T-1:-1:1
+    for t = T:-1:2
         [allStates, numStates] = enumStates(t,arms);
         for i=1:numStates
-           for j=1:4
+            fprintf('At state : %d,%d,%d,%d',allStates(i,1),allStates(i,2),allStates(i,3),allStates(i,4));
+            %allStates(i)
+            for j=1:4
                if allStates(i,j) == 0
                    continue;
                end
@@ -40,7 +43,7 @@ function [decisions,rewards, mu1, mu2] = optimalModel(T,arms,alpha,beta)
                idxS2 = vector(:,2);
                idxF1 = vector(:,3);
                idxF2 = vector(:,4);             
-               
+
                totalArm1 = s1+f1+alpha+beta;
                totalArm2 = s2+f2+alpha+beta;
                %sample Reward From Mu    
@@ -48,21 +51,25 @@ function [decisions,rewards, mu1, mu2] = optimalModel(T,arms,alpha,beta)
                arm2Reward = binornd(1, mu2);
                arm1CurrentReward = 0;
                arm2CurrentReward = 0 ;
-               
+
                %if(arm1Reward == 1)
                %    arm1CurrentReward = (s1+alpha)/(totalArm1);
                %end
                %if(arm2Reward == 1)
                %    arm2CurrentReward = (s2+alpha)/(totalArm2);
                %end
-               
-                   
+
+
                arm1Value = ((s1+alpha)/(totalArm1)) * valueMatrix(s1+1,idxS2,idxF1,idxF2) + ((f1+beta)/(totalArm1)) * valueMatrix(idxS1,idxS2,f1+1,idxF2) + arm1CurrentReward;
-               
+
                arm2Value = ((s2+alpha)/(totalArm2)) * valueMatrix(idxS1,s2+1,idxF1,idxF2) + ((f2+beta)/(totalArm2)) * valueMatrix(idxS1,idxS2,idxF1,f2+1) + arm2CurrentReward;
-               
+
                [valueMatrix(idxS1,idxS2,idxF1,idxF2), decisionMatrix(idxS1,idxS2,idxF1,idxF2)] = max([arm1Value,arm2Value]); 
-           end  
+               %max([arm1Value,arm2Value])
+               
+               fprintf('Setting value function of state : %d,%d,%d,%d to value = %f\n',idxS1,idxS2,idxF1,idxF2, valueMatrix(idxS1,idxS2,idxF1,idxF2));
+               
+            end  
         end
     end
     [decisions,rewards] = getDecisionVector1(T,valueMatrix);
